3206aa428ce208bdbaa09d13ebc952ae0fe007d4
HuanXefh <huanxefh@gmail.com>
2026-02-16 19:00:48 +0800

    v101
    Release of v101.

--- scripts/ct/CT_ENV_depthOre.js
+++ scripts/ct/CT_ENV_depthOre.js
@@ -24,6 +24,11 @@
   /* copper */
 
 
+  const env0ore0dpore_chalcopyrite = extendBase(
+    TEMPLATE, "env0ore0dpore-chalcopyrite",
+  );
+
+
   const env0ore0dpore_nativeCopper = extendBase(
     TEMPLATE, "env0ore0dpore-native-copper",
   );
@@ -53,6 +58,25 @@
 
   const env0ore0dpore0lvl1_rawCoal = extendBase(
     TEMPLATE, "env0ore0dpore0lvl1-raw-coal",
+    TEMPLATE.build({
+      depthLvl: 1,
+    }),
+  );
+
+
+  /* iron */
+
+
+  const env0ore0dpore0lvl1_hematite = extendBase(
+    TEMPLATE, "env0ore0dpore0lvl1-hematite",
+    TEMPLATE.build({
+      depthLvl: 1,
+    }),
+  );
+
+
+  const env0ore0dpore0lvl1_magnetite = extendBase(
+    TEMPLATE, "env0ore0dpore0lvl1-magnetite",
     TEMPLATE.build({
       depthLvl: 1,
     }),
